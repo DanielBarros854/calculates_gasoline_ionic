1bac8581e196d58251a7e31576c34efc79884f7a
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-06-02 22:47:42 -0300

    feat: initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public precoGasolina: number;
+  public precoAlcool: number;
+  public resultado: string;
+  public resultado2: number;
+  private porcentagemConta = 0.7;
+
   constructor() {}
 
+  public calcular() {
+    if (
+      this.precoAlcool/this.precoGasolina < this.porcentagemConta
+    ) {
+      this.resultado = 'Abasteça com álcool!';
+    } else {
+      this.resultado = 'Abasteça com gasolina!';
+    };
+  }
 }
